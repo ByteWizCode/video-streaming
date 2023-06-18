f2a9dc600c94bf609a6d8fb9e8ca3aab45fccf10
ByteWizCode <136901319+ByteWizCode@users.noreply.github.com>
2023-06-18 22:07:48 +0700

    Update show.blade.php

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,31 +1,32 @@
 @extends('videos.layout')
-  
+
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> Show Videos</h2>
+    <div class="row justify-content-center gap-3">
+        <div class="col-12 col-md-6">
+            <div class="d-flex align-items-center justify-content-between py-4">
+                <h2 class="h2">Show Video</h2>
+                <a class="btn btn-link" href="{{ route('videos.index') }}"> Back</a>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('videos.index') }}"> Back</a>
+
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Name</strong>
+                        {{ $video->name }}
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="ratio ratio-16x9">
+                        <video class="w-100" controls>
+                            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/mp4">
+                            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/mp3">
+                            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/ogg">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-   
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {{ $video->name }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-        <video width="100%" height="500" controls>
-            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/mp4">
-            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/mp3">
-            <source src="{{ asset('storage/posts/' . $video->source) }}" type="video/ogg">
-            Your browser does not support the video tag.
-        </video>
         </div>
     </div>
 @endsection
