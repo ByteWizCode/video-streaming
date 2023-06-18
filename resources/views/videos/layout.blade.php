13de7c651f7b50db851dff63f99f8071a2283e70
ByteWizCode <136901319+ByteWizCode@users.noreply.github.com>
2023-06-18 22:07:16 +0700

    Update layout.blade.php

--- resources/views/videos/layout.blade.php
+++ resources/views/videos/layout.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 10 CRUD Application - ItSolutionStuff.com</title>
+    <title>Video Stream</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     
-<div class="container">
+<div class="container py-5">
     @yield('content')
 </div>
     
